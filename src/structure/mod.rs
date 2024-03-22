df496d30c0418becbbd1a9c5bf7aa5568100c609
Steinar <steinar.simonnes@gmail.com>
2024-03-22 15:33:50 +0100

    Add generic trait for weights, to avoid having to write a bajillion traits whenever we have a generic number

--- src/structure/mod.rs
+++ src/structure/mod.rs
@@ -2,3 +2,4 @@ pub mod graph;
 pub mod undirected_graph;
 pub mod cost;
 pub mod path_result;
+pub mod weight;
---
+++ src/structure/weight.rs
@@ -0,0 +1,37 @@
+use std::ops::{Add, Div, Sub};
+use std::str::FromStr;
+use std::fmt::{Debug, Display};
+
+pub trait Weight:
+    Add<Output = Self> +
+    Sub<Output = Self> +
+    Div<Output = Self> +
+    Clone +
+    PartialEq +
+    FromStr<Err: Display + Debug> +
+    From<u64> +
+    PartialOrd +
+    Ord +
+    Copy +
+    Default +
+    Debug +
+    Display +
+{}
+
+impl<T> Weight for T
+    where
+        T:
+            Add<Output = T> +
+            Sub<Output = T> +
+            Div<Output = T> +
+            Clone +
+            PartialEq +
+            FromStr<Err: Display + Debug> +
+            From<u64> +
+            PartialOrd +
+            Ord +
+            Copy +
+            Default +
+            Debug +
+            Display,
+{}

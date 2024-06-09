9086a12cbd3172d8f16c55a35f409ee56fd92da9
Steinar <steinar.simonnes@gmail.com>
2024-06-09 17:53:40 +0200

    Add python scripts to automate benching of paths and walks

---
+++ benches/scripts/bench_delaunay.py
@@ -0,0 +1,29 @@
+import json
+from subprocess import run, PIPE
+
+output = (run("cargo criterion path --message-format=json", shell=True, stdout=PIPE)
+          .stdout
+          .decode('utf-8')
+          .splitlines())
+
+sizes = []
+times = []
+units = set()
+for out in output:
+    if out.startswith('{'):
+        dct = json.loads(out)
+        if dct['reason'] == 'benchmark-complete' and 'delaunay' in dct['id']:
+            i = dct['id'].index('/delaunay') + len('/delaunay')
+            values = dct['measured_values']
+            time = dct['mean']['estimate']
+            sizes.append(int(dct['id'][i:]))
+            times.append(time)
+            units.add(dct['unit'])
+
+print(f"sizes = {sizes}")
+print(f"times = {times}")
+print(f"units = {units}")
+
+
+# dec = json.loads(test)
+# print(f"Time spent on {dec["id"].split()[-1][6:]}: {min(dec["measured_values"])}{dec["unit"]}")
